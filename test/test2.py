35f80303f06585a9ae77d7c656a40b7e4118f177
dregor <dregor614@bk.ru>
2013-02-04 23:38:26 +0300

    Избавился от своперов и укарасил код

--- test/test2.py
+++ test/test2.py
@@ -1,15 +1,24 @@
 from test import *
 
-def kub(a,b):
-	for x in xrange(a,b):
-		print '%2d %3d %4d' % (x,x*x,x*x*x)
-def TStr():
+def TEST():
 	x=10*3.14
 	print str(x)
 	print repr(x)
-	print str('test \n string \n')
-	print `'test \n string \n'`
+	print str('test\nstring\n')
+	print `'test \nstring \n'`,'\n'
 	print max([34,556,234,22]),'\n'
-	kub(1,13)
-	
-TStr()
+	kub(1,13),'\n'
+	form(),'\n'
+
+def kub(a,b):
+	for x in xrange(a,b):
+		print '%2d %3d %4d' % (x,x*x,x*x*x)
+
+def form():
+	table={'Joe':2122,
+	       'Jack':4765,
+	       'Duck':3432}
+	for name,phone in table.items():
+		print '%10s |-> %10d' %(name,phone)
+	print '\n',vars(),'\n'
+TEST()
