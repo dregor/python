060ae3417d42a9ef130e60e9b0f6f1fb99be55a0
dregor <dregor614@bk.ru>
2013-02-12 21:02:36 +0300

    klass - add

--- test/class.py
+++ test/class.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 import os,sys,io
 
+
+
 class Person:
     def __init__(self, name, age, pay=0, job=None):
         self.name=name
@@ -8,18 +10,34 @@ class Person:
         self.pay=pay
         self.job=job
 
-if __name__ == '__main__':
-    print( 'Параметров -',str(len(sys.argv) ) )
-    if( len(sys.argv)>3 ):
-        man = Person( sys.argv[1], sys.argv[2] )
+    def print():
         print( str(man) + "\n" )
-        print( "Имя -",man.name )
-        print( "Возраст -",man.age )
-        if(len(sys.argv)>4):
-            man.pay = sys.argv[3]
-        if(len(sys.argv)>5):
-            man.pay = sys.argv[4]
-        print( "Зарплата -",man.pay )
-        print( "Работа -",man.job )
+        print( "Имя -",self.name )
+        print( "Возраст -",self.age )
+        print( "Зарплата -",self.pay )
+        print( "Работа -",self.job )
+    
+    def save():
+
+    def read():
+
+
+
+if __name__ == '__main__':
+    man=None
+    print( 'Параметров -',str(len(sys.argv) ) )
+    if( len(sys.argv)==1 ):
+        sys.argv=['class.py','man','100']
+    if( len(sys.argv)==2 ):
+        print('В разработке!')
+    elif( len(sys.argv)==3 ):
+        man = Person( sys.argv[1], sys.argv[2] )
+    elif( len(sys.argv)==4 ):
+        man = Person( sys.argv[1], sys.argv[2], sys.argv[2]  )
+    elif( len(sys.argv)==5 ):
+        man = Person( sys.argv[1], sys.argv[2] , sys.argv[3] , sys.argv[4] )
     else:
         print('Add men: name age [pay] [job] .')
+
+    if man is not None:
+        man.print()
