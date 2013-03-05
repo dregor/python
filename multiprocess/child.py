90fe99eb309d95b3c5d9f09206236014327af535
dregor <dregor614@bk.ru>
2013-03-05 09:36:55 +0300

    ничего вроде не делал

--- multiprocess/child.py
+++ multiprocess/child.py
@@ -1,10 +1,10 @@
-
+#! /usr/bin/env python3
 from multiprocessing import Process, Pipe
 import time,os
 
 def f(conn):
     a = 'МОДУЛЬ : '+ __name__+ '\n'
-    if hasattr(os, 'getppid'):  # only available on Unix
+    if hasattr(os, 'getppid'):
         a += 'РОДИТЕЛЬ : '+ str(os.getppid()) + '\n'
     a += 'id ПРОЦЕССА : '+ str(os.getpid()) + '\n'
     timeend = time.time()
