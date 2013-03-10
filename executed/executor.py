481b027d0d184492147d3bff834d3181c1fc49cc
dregor <dregor614@bk.ru>
2013-03-10 10:40:11 +0300

    Екзекутор все классы пашут, делаю многопроцессовость

--- executed/executor.py
+++ executed/executor.py
@@ -1,32 +1,40 @@
 
-def execute( arch_desc, queue_file ):
-    while True:
-        try:
-            file_name = self.queue_file.get( block = True )
-        except queue_file.Empty:
-            pass
-        else:
-            arch_desc.extract( file_name )
-
 class Arch():
 
     def __init__( self, name, path = None ):
         from os import path
-        from Queue import Queue
+        from multiprocessing import Queue, Process
         self.name = name
         self.queue_file = Queue()
         if path == None:
             self.path = path.dirname( __file__ )
         else:
             self.path = path
+        self.runing = True
+        self.process_list = Process
 
     def __str__(self):
         return self.name
+
+    def stop( self ):
+        self.run = False
+
+    def execute( self ):
+        while true:
+            try:
+                file_name = self.queue_file.get( block = True )
+            except queue_file.Empty:
+                os._exit()
+            else:
+                arch_desc.extract( file_name )
+
 
     def get_queue( self ):
         i = 0
         for i in range( self.queue_file.qsize().__int__() ):
             print( self.queue_file.get( block=False ) )
+
+
 
 class ZipArch( Arch ):
     def __init__( self, name, path = None ):
@@ -52,7 +60,7 @@ class TarArch( Arch ):
         elif name[-8:] == '.tar.bz2' :  readstate = 'r:bz2'
         else : readstate = 'r:tar'
         self.arch_desc = tarfile.open( name , readstate )
-        for item in self.arch_desc.members:
+	for item in self.arch_desc.getmembers:
             self.queue_file.put( item.name )
 
 
