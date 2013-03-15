cb2feeb7817be85e5d88a4c0e7c004c91b27eb6e
dregor <dregor614@bk.ru>
2013-03-15 23:25:02 +0300

    Изменил принцип процессов

--- executed/executor.py
+++ executed/executor.py
@@ -2,6 +2,7 @@ class Arch():
 
     def __init__( self, name, path = None ):
         from os import path
+        self.name = name
         if path == None:
             self.path = path.dirname( __file__ )
         else:
@@ -16,29 +17,42 @@ class Arch():
     def extract( self, file_name ):
            self.arch_desc.extract( file_name )
 
+
 class ZipArch( Arch ):
+
     def __init__( self, name, path = None ):
-        from zipfile import ZipFile
         Arch.__init__( self, name , path )
-        self.arch_desc = ZipFile( name, 'r' )
+        self.open()
+
+    def open( self ):
+        from zipfile import ZipFile
+        self.arch_desc = ZipFile( self.name, 'r' )
 
     def __str__( self ):
         return self.arch_desc.filename
 
 class RarArch( Arch ):
+
     def __init__( self, name, path = None ):
-        from rarfile import RarFile
         Arch.__init__( self, name , path )
-        self.arch_desc = RarFile( name, 'r' )
+        self.open()
+
+    def open( self ):
+        from rarfile import RarFile
+        self.arch_desc = RarFile( self.name, 'r' )
 
 class TarArch( Arch ):
+
     def __init__( self, name, path = None ):
-        import tarfile
         Arch.__init__( self, name , path )
-        if name[-7:] == '.tar.gz' : readstate = 'r:gz'
-        elif name[-8:] == '.tar.bz2' :  readstate = 'r:bz2'
+        self.open()
+
+    def open( self ):
+        import tarfile
+        if self.name[-7:] == '.tar.gz' : readstate = 'r:gz'
+        elif self.name[-8:] == '.tar.bz2' :  readstate = 'r:bz2'
         else : readstate = 'r:tar'
-        self.arch_desc = tarfile.open( name , readstate )
+        self.arch_desc = tarfile.open( self.name , readstate )
 
     def name_list( self ):
         return [ i.name for i in self.arch_desc.getmembers() ]
--- executed/exed.py
+++ executed/exed.py
@@ -1,38 +1,45 @@
-def a( queue, arch ):
-    import os, sys, time
-    while not queue.empty():
-        file = queue.get( block = True )
-        print( file + ' - ' + os.getpid().__str__() )
-        arch.extract( file )
-    sys.exit(0)
+from multiprocessing import Process
+
+class ExedProc( Process ):
+    def __init__( self, queue, arch ):
+        Process.__init__( self )
+        self.queue = queue
+        self.arch = arch
+
+    def run( self ):
+        import os
+        while not self.queue.empty():
+            file = self.queue.get( block = True )
+            print( file + ' - ' + os.getpid().__str__() )
+            self.arch.extract( file )
+        os._exit(0)
+
 
 class Exed():
 
     def __init__( self, name, path = None, proc = None):
-        from multiprocessing import Queue, Process, Pool, Manager, cpu_count
+        from multiprocessing import Queue, cpu_count
         from executor import TarArch, RarArch, ZipArch
 
-        if name[-8:] == '.tar.bz2' or name[-4:] == '.tar' or name[-7] == '.tar.gz':
+        if name[-8:] == '.tar.bz2' or name[-4:] == '.tar' or name[-7:] == '.tar.gz':
             self.arch = TarArch( name )
         elif name[-4:] == '.zip':
             self.arch =ZipArch( name )
         elif name[-4:] == '.rar':
             self.arch = RarArch( name )
 
-        #m = Manager()
         self.queue = Queue()
 
         for item in self.arch.name_list():
             self.queue.put( item )
 
-        #self.pool = Pool( processes = 4 )
-
         if proc == None:
             self.proc = cpu_count()
         else:
             self.proc = proc
+        #self.proc = 1
 
-        self.proc_list = [ Process(target=a, args=(self.queue,self.arch)) for item in range( self.proc )]
+        self.proc_list = [ ExedProc( self.queue, self.arch ) for item in range( self.proc )]
 
     def stop( self ):
         self.runing = False
@@ -40,22 +47,12 @@ class Exed():
             item.terminate()
 
     def start( self):
-        #import pdb
-        #pdb.set_trace()
-        #self.pool.apply_async( a, args = ( self.queue , self.arch))
         self.runing = True
         for item in self.proc_list:
             item.start()
             print(item)
 
-    def exec( self ):
-        while not queue.empty():
-            file = queue.get( block = True )
-            print( file + ' - ' + os.getpid().__str__() )
-            arch.extract( file )
-        sys.exit(0)
-
 if __name__ == '__main__':
-    import os
-    test = Exed( os.path.dirname( __file__ ) + 'dir.tar' )
+    import os, sys
+    test = Exed( os.path.dirname( __file__ ) + sys.argv[1] )
     test.start()
